c86fa6b4d7bdad8d43735b50054d5d888ed7509a
Alheekmah Library <53709203+alheekmahlib@users.noreply.github.com>
2024-05-03 16:54:36 -0700

    added Adhan

--- android/app/src/main/kotlin/com/alheekmah/alquranalkareem/alquranalkareem/MainActivity.kt
+++ android/app/src/main/kotlin/com/alheekmah/alquranalkareem/alquranalkareem/MainActivity.kt
@@ -2,5 +2,4 @@ package com.alheekmah.alquranalkareem.alquranalkareem
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
